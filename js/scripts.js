bc114b38734590ce8261222b3f930ca332caf9cd
Chris Huson and Natalia Telpukhova <natalia@>
2017-11-30 10:00:37 -0800

    add functionality to buttons, add text to paragraphs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-  
-})
+  $(".btn").click(function() {
+
+    var animal = $("input:radio[name=animal]:checked").val();
+
+
+    if (animal === "turtles") {
+      $("#turtles").show();
+      $("#snakes").hide();
+      $("#insects").hide();
+    }
+
+    if (animal === "snakes") {
+      $("#turtles").hide();
+      $("#snakes").show();
+      $("#insects").hide();
+    }
+
+    if (animal === "insects") {
+      $("#turtles").hide();
+      $("#snakes").hide();
+      $("#insects").show();
+    }
+
+  });
+
+});
